18e93ab69b5c3c4703b0b31894962bbd552a769f
pinyuho <pollyho42@gmail.com>
2024-04-05 02:30:02 +0800

    [TAS-29] Update menu base on comments

--- frontend/src/component/NavbarItems.js
+++ frontend/src/component/NavbarItems.js
@@ -1,33 +1,42 @@
 import React from 'react';
-import * as IoIcons from 'react-icons/io5';
-import * as TbIcons from "react-icons/tb";
-import * as MdIcons from "react-icons/md";
-import * as HiIcons from "react-icons/hi";
+import { IoLanguage } from 'react-icons/io5';
+import { TbRobotFace } from "react-icons/tb";
+import { MdOutlineTravelExplore, MdModeOfTravel } from "react-icons/md";
+import { HiDotsHorizontal } from "react-icons/hi";
+
 
 export const NavbarItems = [
   {
-    title: '探索旅程',
+    tabname: 'explore',
+    entitle: 'Explore',
+    zhtitle: '探索旅程',
     path: '/explore',
-    icon: <MdIcons.MdOutlineTravelExplore />
+    icon: <MdOutlineTravelExplore />
   },
   {
-    title: 'AI 助理',
+    tabname: 'assistant',
+    entitle: 'AI Assistant',
+    zhtitle: 'AI 助理',
     path: '/assistant',
-    icon: <TbIcons.TbRobotFace />
+    icon: <TbRobotFace />
   },
   {
-    title: '我的旅程',
+    tabname: 'mytrips',
+    entitle: 'My Trips',
+    zhtitle: '我的旅程',
     path: '/mytrips',
-    icon: <MdIcons.MdModeOfTravel />
+    icon: <MdModeOfTravel />
   },
   {
-    title: '其他',
+    tabname: 'others',
+    entitle: 'Others',
+    zhtitle: '其他',
     path: '/others',
-    icon: <HiIcons.HiDotsHorizontal />
+    icon: <HiDotsHorizontal />
   },
-  {
-    title: '中英切換',
-    path: '/others',
-    icon: <IoIcons.IoLanguage />
-  }
+  // {
+  //   title: '中英切換',
+  //   path: '/others',
+  //   icon: <IoLanguage />
+  // }
 ];
