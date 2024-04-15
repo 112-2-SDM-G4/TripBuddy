1d0bbbf90ac1c2d0592ce3074c7737ea5171d8b0
r12725034 <46043349+chiao0418@users.noreply.github.com>
2024-04-15 23:20:52 +0800

    Revert "Merge branch 'main' into backend-dev"
    This reverts commit 64f52af2cd5ba14557973612a239866073f6e7e2, reversing
    changes made to 2b2472613886edeed37da501bb2e7e2f1c3914ad.

--- frontend/src/page/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./NotFound.module.css";
-
-const NotFound = () => {
-    return <div className={style.page}>內容不存在</div>;
-};
-
-export default NotFound;
