726701006a9ffecbd172c4a5311a274528a55920
pinyuho <pollyho42@gmail.com>
2024-04-16 16:33:42 +0800

    [TAS-44] Add loader

--- frontend/src/page/MyTrips.jsx
+++ frontend/src/page/MyTrips.jsx
@@ -7,89 +7,60 @@ import { fetchWithJwt } from "../hooks/fetchWithJwt";
 import style from "./MyTrips.module.css";
 import TripCard from "../component/TripCard";
 import Button from "../component/Button";
+import Loader from "../component/Loader";
 
 
-const Explore = () => {
+const MyTrips = () => {
     const navigate = useNavigate();
     const { language } = useLanguage();
+    const [isLoading, setIsLoading] = useState(true);
     const [trips, setTrips] = useState([
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        }, 
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        },
-        {
-            "id": "234",
-            "name": "trip nameee",
-            "image": "https://",
-        }
+        // {
+        //     "id": "234",
+        //     "name": "trip nameee",
+        //     "image": "https://",
+        // }
     ]);
 
     useEffect(() => {
-        // fetchWithJwt("/api/v1/trip", "GET")
-        //     .then(function (response) {
-        //         console.log(response);
-        //         return response.json();
-        //     })
-        //     .then(function (result) {
-        //         console.log(result);
-        //         if (result["user_trip"]) {
-        //             setTrips(result["user_trip"]);
-        //         } else {
-        //             alert(result["message"]);
-        //         }
-        //     });
-        // return () => {};
+        fetchWithJwt("/api/v1/trip", "GET")
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (result) {
+                console.log(result);
+                if (result["user_trip"]) {
+                    setTrips(result["user_trip"]);
+                    setIsLoading(false);
+                } else {
+                    alert(result["message"]);
+                }
+            });
+        return () => {};
     }, []);
 
     return (
-        <div className={style.container}>
-            {trips.map((trip) => (
-                <TripCard
-                    key={trip["id"]}
-                    name={trip["name"]}
-                    src={trip["image"]}
-                    tripId={trip["id"]}
-                />
-            ))}
+        <>
+            <Loader isLoading={isLoading} />
+            <div className={style.container}>
+                {trips.map((trip) => (
+                    <TripCard
+                        key={trip["id"]}
+                        name={trip["name"]}
+                        src={trip["image"]}
+                        tripId={trip["id"]}
+                    />
+                ))}
 
-            <div className={style.buttoncontainer}>
-                <div className={style.button}>
-                    <Button txt={language === "en" ? "Add a trip" : "創建行程"} func={() => navigate(`/edit`)}/>
+                <div className={style.buttoncontainer}>
+                    <div className={style.button}>
+                        <Button txt={language === "en" ? "Add a trip" : "創建行程"} func={() => navigate(`/edit`)}/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
-export default Explore;
+export default MyTrips;
