c69eb5b8ae5826739d26c6cba2a3ce49fa07df39
r12725034 <46043349+chiao0418@users.noreply.github.com>
2024-04-29 15:34:18 +0800

    [TAS-71] Modify 1. Tag Name 新增中英版本

--- backend/app/models/tags.py
+++ backend/app/models/tags.py
@@ -4,8 +4,9 @@ class Tags(db.Model):
     __tablename__ = 'Tags'
     tag_id = db.Column(db.Integer, primary_key=True)
     name_zh = db.Column(db.String(50), nullable=False)
-    type = db.Column(db.String(50), nullable=False)
+    type_zh = db.Column(db.String(50), nullable=False)
     name_en = db.Column(db.String(30), nullable=True)
+    type_en = db.Column(db.String(30), nullable=True)
 
     @staticmethod
     def get_by_id(id):
