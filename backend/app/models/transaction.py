632f520ffab509a56f45639bee72c92509ac015e
ubereats666 <32301wwwww@gmail.com>
2024-04-30 02:54:13 +0800

    [TAS-57] Modify: schema changes

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -1,8 +1,8 @@
 from app.models.create_db import db
 
-class Ledger(db.Model):
-    __tablename__ = 'Ledger'
-    ledger_id = db.Column(db.Integer, primary_key=True)
+class Transaction(db.Model):
+    __tablename__ = 'Transaction'
+    transaction_id = db.Column(db.Integer, primary_key=True)
     schedule_id = db.Column(db.Integer, db.ForeignKey('Schedule.schedule_id'), nullable=False)
     item_name = db.Column(db.String(255), nullable=False)
     amount = db.Column(db.Float, nullable=False)
@@ -15,33 +15,33 @@ class Ledger(db.Model):
 
     @staticmethod
     def get_by_id(id):
-        return Ledger.query.get(id)
+        return Transaction.query.get(id)
     
     @staticmethod
     def create(data):
-        ledger = Ledger(
+        transaction = Transaction(
             schedule_id = data['schedule_id'],
             item_name = data['item_name'],
             amount = data['amount'],
             currency = data['currency']
         )
-        db.session.add(ledger)
+        db.session.add(transaction)
         db.session.commit()
-        return ledger
+        return transaction
     
     @staticmethod
     def update(id, data):
-        ledger = Ledger.query.get(id)
-        ledger.schedule_id = data['schedule_id']
-        ledger.item_name = data['item_name']
-        ledger.amount = data['amount']
-        ledger.currency = data['currency']
+        transaction = Transaction.query.get(id)
+        transaction.schedule_id = data['schedule_id']
+        transaction.item_name = data['item_name']
+        transaction.amount = data['amount']
+        transaction.currency = data['currency']
         db.session.commit()
-        return ledger
+        return transaction
     
     @staticmethod
     def delete(id):
-        ledger = Ledger.query.get(id)
-        db.session.delete(ledger)
+        transaction = Transaction.query.get(id)
+        db.session.delete(transaction)
         db.session.commit()
-        return ledger
+        return transaction
