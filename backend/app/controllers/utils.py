11b5b6f6dc7a1336d623816593560de3ee509f92
Jeffery <jeffery5bai@gmail.com>
2024-05-02 21:35:02 +0800

    [TAS-58] Modify refactor the code

--- backend/app/controllers/utils.py
+++ backend/app/controllers/utils.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
 from datetime import datetime, date, timedelta
 
 from app.models.user import User
@@ -8,6 +8,8 @@ from app.models.place import Place
 from app.models.relation_spot_sch import RelationSpotSch
 from app.models.tags import Tags
 from app.models.relation_sch_tag import RelationSchTag
+from app.models.transaction import Transaction
+from app.models.relation_user_transaction import RelationUserTransaction
 from app.services.google import GoogleMapApi
 
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
@@ -98,3 +100,35 @@ def fetch_and_save_place(place_id: str, language: str) -> Tuple[int, Dict] :
         Place.create(new_place)
 
     return res_code, place_detail
+
+def get_all_transactions_of_schedule(schedule_id: int) -> List[Dict]:
+    """Get all transaction records from a schedule"""
+    records = []
+    transactions = Transaction.get_by_schedule(schedule_id)
+    if transactions is None:
+        return [{}]
+    for t in transactions:
+        record = query_row_to_dict(t)
+        d = datetime.strptime(record['date'], "%Y-%m-%d")
+        record['date'] = d.strftime("%b %d")
+        transaction_id = record['transaction_id']
+        payees = []
+        balances = RelationUserTransaction.get_by_transaction(transaction_id)
+        for b in balances:
+            b_record = query_row_to_dict(b)
+            if b_record['pay'] == 'True':
+                record['payer'] = User.get_by_id(b_record['user_id']).email
+                record['payer_name'] = User.get_by_id(b_record['user_id']).user_name
+            else:
+                payee = {
+                    'payee': User.get_by_id(b_record['user_id']).email,
+                    'payee_name': User.get_by_id(b_record['user_id']).user_name,
+                    'borrowed_amount': float(b_record['balance'])
+                }
+                payees.append(payee)
+        record['payees'] = payees
+        record['amount'] = float(record['amount'])
+        record['transaction_id'] = int(record['transaction_id'])
+        record.pop('schedule_id')
+        records.append(record)
+    return records
